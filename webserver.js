27f110f1baa34d9325cfe18604be3522293f63e3
arnaucode <arnaucode@gmail.com>
2017-09-06 13:03:09 +0200

    starting

---
+++ webserver.js
@@ -0,0 +1,5 @@
+connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(80, function(){
+            console.log('Server running on 80...');
+});
